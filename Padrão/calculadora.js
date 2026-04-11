01e0b71a3829b24b0e258ec5b3ddee93e7f9adfd
wendelloliveiras20 <wendelloliveira.sp20@gmail.com>
2026-04-11 16:13:43 +0000

    calculando normal

---
+++ Padrão/calculadora.js
@@ -0,0 +1,12 @@
+function somar(a,b) {return a+b;}
+function subtrair(a,b) {return a-b;}
+function multiplicar(a,b) {return a*b;}
+
+function dividir(a,b){
+    if(b==0)
+        alert("Divisão por Zero!")
+    return a/b;
+}
+function exponencial(a,b){
+    return Math.pow(a,b);
+}
