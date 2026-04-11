910305a00114468714946dac92432e7fd8a388ca
wendelloliveiras20 <wendelloliveira.sp20@gmail.com>
2026-04-11 16:21:36 +0000

    calculando Fibonacchi

---
+++ Fibonacchi/fibo.js
@@ -0,0 +1,14 @@
+const fibonacci = (x) => {
+    if(x==0 || x==1)
+        return 1;
+    let fm1 = 1, fm2 =1, fm;
+    for(let i=2; i<=x;i++){
+        fm = fm1 + fm2;
+        fm2 = fm1;
+        fm1 = fm;
+    }
+    return fm;
+}
+const fiboRec = (x) => {
+    return (x==0 || x==1) ? 1 : (fiboRec(x-1)+ fiboRec(x-2));
+}
